1586d0d43de794c4389802643d23af97b0b6f62c
shussein23 <110112478+shussein23@users.noreply.github.com>
2022-11-12 11:31:04 -0500

    Add files via upload

---
+++ MusicAlly/header.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include_once 'includes/functions.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MusicAlly</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <script src="https://kit.fontawesome.com/a71247737b.js" crossorigin="anonymous"></script>
+  <script src="js\app.js"></script>
+
+</head>
+
+<body>
+  <!-- Navbar -->
+  <nav class="navbar">
+    <a href="home.php" class="navbar__logo">MusicAlly</a>
+    <div class="navbar__toggle" id="mobile-menu">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </div>
+    <div class="navbar__menu">
+      <a href="home.php" class="navbar__link">Home</a>
+      <a href="event.html" class="navbar__link">Event</a>
+      <?php
+      if (isset($_SESSION["useruid"])) {
+        echo '<a href="profile.php" class="navbar__link">Profile</a>';
+        echo '<a href="logout.php" class="navbar__link">Logout</a>';
+      } else {
+        echo '<a href="login.php" class="navbar__link">Musician Login</a>';
+      }
+      ?>
+
+  </nav>
+
+  </div>
+  </nav>
