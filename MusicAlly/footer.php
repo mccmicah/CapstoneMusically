b2bbcb40197fcee1cd8391c00eecceab1671f151
shussein23 <110112478+shussein23@users.noreply.github.com>
2022-12-06 20:48:19 -0500

    Update footer.php

--- MusicAlly/footer.php
+++ MusicAlly/footer.php
@@ -1,26 +1,17 @@
-</div>
-<!-- Footer -->
-<footer class="footer">
-    <div class="footer__container container">
+<body>
 
-        <div class="footer__list">
-            <p>
-                <a href="#" class="footer__link">About</a>
-            </p>
-            <p>
-                <a href="#" class="footer__link">Our Team</a>
-            </p>
-            <p>
-                <a href="#" class="footer__link">Contact</a>
-            </p>
-        </div>
-
-    </div>
-</footer>
+  <!-- Footer -->
+  <footer class="footer" style="text-align:center">
+    <a href="about.php#aboutus" class="footer__link" style="padding:12px"><i class="fas fa-book"></i> ABOUT</a>
+    <a href="about.php#meetus" class="footer__link" style="padding:12px"><i class="fas fa-table"></i> OUR TEAM</a>
+    <a href="about.php#contactus" class="footer__link" style="padding:12px"><i class="fas fa-user"></i> CONTACT</a>
+    <br><br>
+    <a href="#" class="footer__link"><i class="fa fa-arrow-up"></i> To the top</a>
+  </footer>
 
 
-<!-- js -->
-<script src="js\app.js"></script>
+  <!-- js -->
+  <script src="js\app.js"></script>
 
 </body>
 
