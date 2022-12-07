659bf6191fa0eafba372fa9d3a827bdaa1980fe2
shussein23 <110112478+shussein23@users.noreply.github.com>
2022-12-06 20:47:24 -0500

    Create about.php

---
+++ MusicAlly/about.php
@@ -0,0 +1,71 @@
+<?php
+include_once 'header2.php';
+?>
+</div>
+
+<div style="padding:48px" id="about">
+  <div style="text-align:center;padding:48px;background-color:lightgrey">
+    <h1 id="aboutus">About Us</h1><br>
+    <p>Here is our About Us page!</p>
+  </div>
+  <br><br>
+  <div style="text-align:center;padding:48px;background-color:lightgrey">
+    <h1 id="meetus">Meet Our People!</h1><br>
+    <table class="table">
+      <tr>
+        <td><i class="img"></i>IMAGE 1</td>
+        <td><i class="img"></i>IMAGE 2</td>
+        <td><i class="img"></i>IMAGE 3</td>
+        <td><i class="img"></i>IMAGE 4</td>
+        <td><i class="img"></i>IMAGE 5</td>
+      </tr>
+      <tr>
+        <th>Ana</th>
+        <th>Jinna</th>
+        <th>Sadik</th>
+        <th>Arjun</th>
+        <th>Micah</th>
+      </tr>
+      <tr>
+        <td>Title 1</td>
+        <td>Title 2</td>
+        <td>Title 3</td>
+        <td>Title 4</td>
+        <td>Title 5</td>
+      </tr>
+      <tr>
+        <td class="btnabout" style="color:white;background-color:black">Contact Ana</td>
+        <td class="btnabout" style="color:white;background-color:black">Contact Jinna</td>
+        <td class="btnabout" style="color:white;background-color:black">Contact Sadik</td>
+        <td class="btnabout" style="color:white;background-color:black">Contact Arjun</td>
+        <td class="btnabout" style="color:white;background-color:black">Contact Micah</td>
+      </tr>
+    </table>
+  </div>
+  <br><br>
+  <div style="text-align:center;padding:48px;background-color:lightgrey">
+    <h1 id="contactus">Contact Us</h1><br>
+    <h2>
+      <a href="#event" style="color:black"><i class="fas fa-phone"></i> Phone Number</a>
+      <br><br>
+      <a href="#payment" style="color:black"><i class="fas fa-envelope"></i> Email</a>
+      <br><br>
+      <a href="#profile" style="color:black"><i class="fas fa-map"></i> Address</a>
+    </h2>
+  </div>
+  <br><br>
+
+</div>
+
+<footer class="footer" style="text-align:center">
+  <a href="#aboutus" class="footer__link" style="padding:12px"><i class="fas fa-book"></i> ABOUT</a>
+  <a href="#meetus" class="footer__link" style="padding:12px"><i class="fas fa-table"></i> OUR TEAM</a>
+  <a href="#contactus" class="footer__link" style="padding:12px"><i class="fas fa-user"></i> CONTACT</a>
+  <br><br>
+  <a href="#" class="footer__link"><i class="fa fa-arrow-up"></i> To the top</a>
+</footer>
+
+<script src="js\app.js"></script>
+</body>
+
+</html>
